c795d2b62cc1faae27c4b0144cc51416cc7dbdb0
SMH <kua0408@gmail.com>
2023-07-02 23:40:28 +0900

    ..

--- JAVA_COTE/src/DO_IT/Baek/baek_14235_크리스마스_선물.java
+++ JAVA_COTE/src/DO_IT/Baek/baek_14235_크리스마스_선물.java
@@ -13,33 +13,31 @@ public class baek_14235_크리스마스_선물 {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken()); // 거점지 방문 횟수
+		int N = Integer.parseInt(br.readLine()); // 거점지 방문 횟수
 		StringBuffer sb = new StringBuffer();
 
 		PriorityQueue<Integer> myQueue = new PriorityQueue<>(Collections.reverseOrder()); // 오름차순 우선순위 큐
 
 		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			int V = Integer.parseInt(st.nextToken()); // V == 0 선물, v != 거점
 			// 거점일 경우
 			if (V != 0) {
 				for (int j = 0; j < V; j++) {
 					myQueue.offer(Integer.parseInt(st.nextToken()));
-					System.out.println();
 				}
 				// 선물을 줄경우
 			} else if (V == 0) {
 				if (!myQueue.isEmpty()) {
 					int temp = myQueue.poll();
-					sb.append(temp + "\n");
+					sb.append(temp).append("\n");
 				} else if (myQueue.isEmpty()) {
-					sb.append(-1 + "\n");
+					sb.append(-1).append("\n");
 				}
 
 			}
 		}
-		System.out.println(sb);
+		System.out.print(sb.toString());
 
 	}
 
